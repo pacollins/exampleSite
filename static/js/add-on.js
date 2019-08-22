dd135e7cae558d13e8acf846e8d35cf4ce0afa6d
pacollins <thepatrickcollins@gmail.com>
2019-08-21 20:26:58 -0400

    Initial Commit

---
+++ static/js/add-on.js
@@ -0,0 +1,30 @@
+/*
+  This js file is for individual users to modify the scripts for their personal site,
+  or for the implementation of features specifically for their site. Anything that
+  is an official part of the theme (ex. Pull Requests) should be included in main.js
+  and follow the formatting and style given.
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
